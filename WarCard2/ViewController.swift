6c8f2d8bdbeb4805b971ff72ac78937e8d1d84bf
lucky0165 <sape0165@gmail.com>
2021-02-14 17:40:31 +0100

    Initial Commit

---
+++ WarCard2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCard2
+//
+//  Created by Łukasz Rajczewski on 14/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
