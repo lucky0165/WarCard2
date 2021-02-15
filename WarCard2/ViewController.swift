65f7624f8c3e822abfb9f6b286c08175a8f20167
lucky0165 <sape0165@gmail.com>
2021-02-15 17:28:54 +0100

    self made WarCard App from cwc+ with timers and card rotations

--- WarCard2/ViewController.swift
+++ WarCard2/ViewController.swift
@@ -8,12 +8,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftCardImage: UIImageView!
+    @IBOutlet weak var rightCardImage: UIImageView!
+    @IBOutlet weak var leftPoints: UILabel!
+    @IBOutlet weak var rightPoints: UILabel!
+    
+    var leftNumber = 0
+    var rightNumber = 0
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    var timer: Timer?
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    func rotate() {
 
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    
+    }
+    
+    @objc func update() {
 
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        leftCardImage.image = UIImage(named: "card\(leftNumber)")
+        leftCardImage.transform = leftCardImage.transform.rotated(by: .pi)
+        
+        rightCardImage.image = UIImage(named: "card\(rightNumber)")
+        rightCardImage.transform = rightCardImage.transform.rotated(by: .pi)
+        
+        count += 1
+        
+        if count == 10 {
+            timer?.invalidate()
+            count = 0
+            
+            if leftNumber > rightNumber {
+                leftScore += 1
+                leftPoints.text = "\(leftScore)"
+                leftPoints.font = leftPoints.font.withSize(70)
+
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.leftPoints.font = self.leftPoints.font.withSize(40)
+                }
+            
+            
+            } else if leftNumber < rightNumber {
+                rightScore += 1
+                rightPoints.text = "\(rightScore)"
+                rightPoints.font = rightPoints.font.withSize(70)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.rightPoints.font = self.rightPoints.font.withSize(40)
+                }
+            }
+            
+        }
+    }
+    
+
+    @IBAction func dealButtonTapped(_ sender: UIButton) {
+        
+        rotate()
+
+        
+    }
+    
 }
 
